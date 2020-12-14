577b53b2889fa3ad779a42692b93a5a7e29775fd
Blaine Richardson <blainerrichardson@gmail.com>
2020-12-13 18:22:37 -0600

    chore(sendEmail): adding one more e2e test for sendEmail mutation that returns the email information used to send said email (in addition to the messageId)

--- test/e2e/api/email/resolvers/Email/POST_graphql_mutation_sendEmail.e2e.test.ts
+++ test/e2e/api/email/resolvers/Email/POST_graphql_mutation_sendEmail.e2e.test.ts
@@ -101,7 +101,6 @@ describe('api/utility/resolvers/UtilityHealth.resolver - POST /graphql query hea
 
         // set up expectations
         const EXPECTED_TYPE_OF_STRING = 'string';
-        // const EXPECTED_EMAIL = [email].slice()[0];
 
         /////////////////////////
         //////// test //////////
@@ -145,6 +144,109 @@ describe('api/utility/resolvers/UtilityHealth.resolver - POST /graphql query hea
         expect(parsedBody.data.sendEmail !== undefined).to.be.true;
         expect(parsedBody.data.sendEmail.messageId !== undefined).to.be.true;
         expect(typeof parsedBody.data.sendEmail.messageId === EXPECTED_TYPE_OF_STRING).to.be.true;
+
+        // return explicitly
+        return;
+      } catch (err) {
+        // throw explicitly
+        throw err;
+      }
+    });
+
+    it('- shoud send a new email to the given recipients with the given options', async () => {
+      try {
+        /////////////////////////
+        //////// setup //////////
+        /////////////////////////
+        // set up data for test
+        const email = {
+          from: {
+            address: e2eTestEnv.EMAIL_CLIENT_GMAIL_USERNAME,
+          },
+          to: [
+            {
+              address: e2eTestEnv.TEST_TO_EMAIL_ADDRESS,
+            },
+          ],
+          subject: 'TEST',
+          text: 'TEST EMAIL!',
+        };
+
+        // set up expectations
+        const EXPECTED_TYPE_OF_STRING = 'string';
+        const EXPECTED_EMAIL = [email].slice()[0];
+        const EXPECTED_ARRAY_CLASS_INSTANCE = Array;
+
+        /////////////////////////
+        //////// test //////////
+        /////////////////////////
+
+        // run testee
+        const httpRequest: any = {
+          method: 'POST',
+          url: '/graphql',
+          headers: {
+            'content-type': 'application/json',
+          },
+          payload: {
+            query: `mutation sendEmail($data: SendEmailInputType!) {
+              sendEmail(data: $data) {
+                messageId,
+                email {
+                  from {
+                    name,
+                    address
+                  },
+                  to {
+                    name,
+                    address
+                  },
+                  subject,
+                  text
+                }
+              }
+            }`,
+            variables: {
+              data: {
+                email,
+              },
+            },
+          },
+        };
+        const httResponse = await app.inject(httpRequest);
+
+        // run assertions
+        expect(httResponse !== undefined).to.be.true;
+        expect(httResponse.statusCode !== undefined).to.be.true;
+        expect(httResponse.statusCode === 200).to.be.true;
+        expect(httResponse.body !== undefined).to.be.true;
+        expect(typeof httResponse.body === EXPECTED_TYPE_OF_STRING).to.be.true;
+
+        // parse JSON body
+        const parsedBody = JSON.parse(httResponse.body);
+
+        // validate results
+        expect(parsedBody !== undefined).to.be.true;
+        expect(parsedBody.data !== undefined).to.be.true;
+        expect(parsedBody.data.sendEmail !== undefined).to.be.true;
+        expect(parsedBody.data.sendEmail.messageId !== undefined).to.be.true;
+        expect(typeof parsedBody.data.sendEmail.messageId === EXPECTED_TYPE_OF_STRING).to.be.true;
+        expect(parsedBody.data.sendEmail.email !== undefined).to.be.true;
+        expect(parsedBody.data.sendEmail.email.from !== undefined).to.be.true;
+        expect(parsedBody.data.sendEmail.email.from.name === '').to.be.true;
+        expect(parsedBody.data.sendEmail.email.from.address !== undefined).to.be.true;
+        expect(parsedBody.data.sendEmail.email.from.address === EXPECTED_EMAIL.from.address).to.be.true;
+        expect(parsedBody.data.sendEmail.email.to !== undefined).to.be.true;
+        expect(parsedBody.data.sendEmail.email.to instanceof EXPECTED_ARRAY_CLASS_INSTANCE).to.be.true;
+        for (const item of parsedBody.data.sendEmail.email.to) {
+          const found = EXPECTED_EMAIL.to.find((expecetedEmailToItem: any) => expecetedEmailToItem.address === item.address);
+          expect(found !== undefined).to.be.true;
+          expect(item.name === '').to.be.true;
+        }
+        expect(parsedBody.data.sendEmail.email.subject !== undefined).to.be.true;
+        expect(parsedBody.data.sendEmail.email.subject === EXPECTED_EMAIL.subject).to.be.true;
+        expect(parsedBody.data.sendEmail.email.text !== undefined).to.be.true;
+        expect(parsedBody.data.sendEmail.email.text === EXPECTED_EMAIL.text).to.be.true;
 
         // return explicitly
         return;
