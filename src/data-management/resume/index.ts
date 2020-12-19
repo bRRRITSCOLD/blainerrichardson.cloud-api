08efd0a4a7a0af1b1d3d2a7edb8e9fa20300c9a0
Blaine Richardson <blainerrichardson@gmail.com>
2020-12-18 18:08:18 -0600

    feat(SchoolExperience): adding data-management layer for resume SchoolExperience for replace or creating school experience instances

--- src/data-management/resume/index.ts
+++ src/data-management/resume/index.ts
@@ -1,3 +1,6 @@
 export * from './searchCertifications';
 export * from './searchWorkExperiences';
 export * from './searchSchoolExperiences';
+// export * from './putCertifications';
+// export * from './putWorkExperiences';
+export * from './putSchoolExperiences';
--- src/models/resume/SchoolExperience.ts
+++ src/models/resume/SchoolExperience.ts
@@ -1,10 +1,22 @@
 // node_modules
 import * as _ from 'lodash';
-import { Field, ObjectType } from 'type-graphql';
+import { Field, InputType, ObjectType } from 'type-graphql';
 import * as yup from 'yup';
+import { v4 as uuid } from 'uuid';
+
+// libraries
 import { dateUtils } from '../../lib/utils/date';
 
-@ObjectType()
+export interface SchoolExperienceCompanyAddressInterface {
+  addressLine1: string;
+  addressLine2?: string;
+  city: string;
+  state: string;
+  zipCode: string;
+}
+
+@ObjectType('SchoolExperienceCompanyAddressObjectType')
+@InputType('SchoolExperienceCompanyAddressInputType')
 export class SchoolExperienceCompanyAddress {
   @Field()
   addressLine1: string;
@@ -22,7 +34,16 @@ export class SchoolExperienceCompanyAddress {
   zipCode: string;
 }
 
+export const schoolExperienceAddressSchema = yup.object().shape({
+  addressLine1: yup.string().required(),
+  addressLine2: yup.string().optional(),
+  city: yup.string().required(),
+  state: yup.string().required(),
+  zipCode: yup.string().required(),
+});
+
 export interface SchoolExperienceInterface {
+  schoolExperienceId: string;
   startDate: string;
   endDate?: string;
   schoolName: string;
@@ -32,14 +53,27 @@ export interface SchoolExperienceInterface {
 }
 
 export const schoolExperienceSchema = yup.object().shape({
-  startDate: yup.date().required(),
-  endDate: yup.date().optional(),
-  name: yup.string().required(),
-  institution: yup.string().required(),
+  schoolExperienceId: yup.string().required(),
+  startDate: yup
+    .mixed()
+    .test('is-date', '${path} is not a valid date', (value, _context) => dateUtils.isValid(value))
+    .required(),
+  endDate: yup
+    .mixed()
+    .test('is-date', '${path} is not a valid date', (value, _context) => dateUtils.isValid(value))
+    .optional(),
+  schoolName: yup.string().required(),
+  schoolAddress: schoolExperienceAddressSchema,
+  degree: yup.string().required(),
+  classes: yup.array().of(yup.string().optional()).required(),
 });
 
-@ObjectType()
+@ObjectType('SchoolExperienceObjectType')
+@InputType('SchoolExperienceInputType')
 export class SchoolExperience implements SchoolExperienceInterface {
+  @Field()
+  schoolExperienceId: string;
+
   @Field()
   startDate: string;
 
@@ -60,6 +94,7 @@ export class SchoolExperience implements SchoolExperienceInterface {
 
   public constructor(schoolExperience: Partial<SchoolExperienceInterface>) {
     _.assign(this, schoolExperience, {
+      schoolExperienceId: _.get(schoolExperience, 'schoolExperienceId', uuid()),
       startDate: dateUtils.dateTime(new Date(_.get(schoolExperience, 'startDate', new Date()))),
       endDate: _.get(schoolExperience, 'endDate') ? dateUtils.dateTime(new Date(_.get(schoolExperience, 'endDate') as any)) : undefined,
       schoolName: _.get(schoolExperience, 'schoolName'),
